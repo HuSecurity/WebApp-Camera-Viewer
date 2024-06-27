adc8b1db3839fb74b47172b7ed2fb6424cd60120
Jauhar Goga <jauhargoga@gmail.com>
2024-06-28 03:51:37 +1200

    Worked on login

---
+++ components/styles.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { View, Text, Image } from "react-native";
+
+const StatusBarHeight = Constants.statusBarHeight;
+
+// Colours
+export const Colours = {
+  primary: "affffffff",
+  secondary: "#E5E7EB",
+  teritary: "#1F2937",
+  darklight: "#9CA3AF",
+  brand: "#6d2BD9",
+  green: "#10B981",
+  red: "#EF4444",
+};
+
+const { primary, secondary, teritary, darklight, brand, green, red } = Colours;
+
+export const StyledContainer = styled.view({
+  flex: 1,
+  padding: "25px",
+  "padding-top": `$(StatusBarHeight + 10)px`,
+  "background-colour": `${primary}`,
+});
+
+export const InnerContainer = styled.view({
+  flex: 1,
+  width: "100%",
+  "align-items": "center",
+});
+
+export const PageLogo = styled.image({
+  flex: 1,
+  width: "200px",
+  heigth: "200px",
+});
+
+export const PageTitle = styled.text({
+  "font-size": "30px",
+  "text-align": "center",
+  "font-weight": "bold",
+  colour: $(brand),
+  padding: "10px",
+});
---
+++ screens/login.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle,
+} from "./../components/styles";
+
+const Login = () => {
+    return (
+        <StyledContainer>
+            <InnerContainer>
+                <PageLogo source={requrie("./../")}>
+                    
+                </PageLogo>
+            </InnerContainer>
+        </StyledContainer>
+    );
+
+}
+
+export default Login;
