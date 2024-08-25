d6e72e774c0a4bd0c915460e8e04a4458d22ad9b
Jauhar Goga <jauhargoga@gmail.com>
2024-08-25 23:35:16 +1200

    Committing before bed
    added prisma adapter, worked on login. There is 2 errors on src/lib/auth.ts that need fixing. maybe tmr

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,40 +1,40 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials"
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { database } from "./database";
+import { compare } from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(database),
+    session: {
+        strategy: 'jwt'
+    },
     pages: {
         signIn: '/sign-in'
     },
     providers: [
         CredentialsProvider({
-          // The name to display on the sign in form (e.g. 'Sign in with...')
+          // The name to display on the sign in form
           name: 'Credentials',
           // The credentials is used to generate a suitable form on the sign in page.
           credentials: {
-            username: { label: "Email", type: "email", placeholder: "mail@example.com" },
+            email: { label: "Email", type: "email", placeholder: "mail@example.com" },
             password: { label: "Password", type: "password" }
           },
-          async authorize(credentials, req) {
-            // You need to provide your own logic here that takes the credentials
-            // submitted and returns either a object representing a user or value
-            // that is false/null if the credentials are invalid.
-            // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-            // You can also use the `req` object to obtain additional parameters
-            // (i.e., the request IP address)
-            const res = await fetch("/your/endpoint", {
-              method: 'POST',
-              body: JSON.stringify(credentials),
-              headers: { "Content-Type": "application/json" }
-            })
-            const user = await res.json()
-      
-            // If no error and we have user data, return it
-            if (res.ok && user) {
-              return user
+          async authorize(credentials) {
+            if(!credentials?.email || !credentials?.password){
+                return null;
             }
-            // Return null if user data could not be retrieved
-            return null
-          }
+
+            const existingUser = await database.user.findUnique({
+                where: {email: credentials?.email}
+            });
+            if(!existingUser) {
+                return null;
+            }
+
+            const passwordMatch = await compare(credentials.password, existingUser.password);
+
         })
       ]
 }
