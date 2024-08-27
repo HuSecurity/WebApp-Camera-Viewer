9940b115103ec125198f9d726e5ba08a08cd2c13
Jauhar Goga <jauhargoga@gmail.com>
2024-08-27 22:10:51 +1200

    Fixed build error

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -4,7 +4,7 @@ import { getServerSession } from "next-auth";
 const page = async () => {
   const session = await getServerSession(authOptions);
   if (session?.user) {
-  return <h2 className="text-2xl text-slate-200">Camera Page - Welcome Back {session.user.username}</h2>
+  return <h2 className="text-2xl text-slate-200">Camera Page - Welcome Back</h2>
   }
 
   return <h2 className="text-2xl text-slate-200">Please login to see this camera</h2>;
